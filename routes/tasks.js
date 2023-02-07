df1c2c17a2a028e4af8be41a7257bb436a61b158
GayanMalinda99 <gayan1999malinda@gmail.com>
2023-02-07 11:47:27 +0530

    Backend completed for Task-keeper application

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,9 +8,8 @@ const tasks = [];
 let id = 0;
 
 //Create a task
-router.post("/tasks", (req, res) => {
+router.post("/tasks", createTaskValidation, (req, res) => {
   try {
-    console.log("create a task");
     const { title, description } = req.body;
     const task = {
       title: title,
@@ -21,7 +20,6 @@ router.post("/tasks", (req, res) => {
     id++;
     res.status(200).json(task);
   } catch (err) {
-    console.log("create a task - err");
     res.status(500).json(err);
   }
 });
@@ -40,7 +38,7 @@ router.get("/tasks", (req, res) => {
 });
 
 //Get a specific task
-router.get("/task/:id", (req, res) => {
+router.get("/task/:id", taskValidation, (req, res) => {
   const id = req.params.id;
   const task = tasks.find((task) => task.id == id);
   if (task) {
@@ -51,7 +49,7 @@ router.get("/task/:id", (req, res) => {
 });
 
 //Edit a specific task
-router.put("/tasks/:id", (req, res) => {
+router.put("/tasks/:id", taskValidation, (req, res) => {
   try {
     const id = req.params.id;
     const { title, description } = req.body;
@@ -73,7 +71,7 @@ router.put("/tasks/:id", (req, res) => {
 });
 
 //Delete a specific task
-router.delete("/tasks/:id", (req, res) => {
+router.delete("/tasks/:id", taskValidation, (req, res) => {
   try {
     const id = req.params.id;
     const task = tasks.find((task) => task.id == id);
