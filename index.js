aae74cd7ba102fa8a353594657ad8d0e75d8dba7
GayanMalinda99 <gayan1999malinda@gmail.com>
2023-02-07 11:31:52 +0530

    Remove body-parser and cors and reimport helmet and morgan

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 const app = express();
-import bodyParser from "body-parser";
-import cors from "cors";
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 import taskRoute from './routes/tasks.js';
 
-app.use("/tasks", taskRoute);
+//Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/", taskRoute);
 
 app.listen(8800, () => {
-  console.log("Backend server is running in port 8800");
+  console.log("Backend server is running in port 8800!");
 });
